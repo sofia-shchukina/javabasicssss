6455ce01be103d0af3a95bf023a77021f3797da7
Sofia Shchukina <sofiashchukina@outlook.com>
2022-06-28 16:08:12 +0200

    smth

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] someArray = {6, 5, 3};
+        System.out.println(someArray[0]);
+        System.out.println(someArray[2]);
+        System.out.println(someArray[0] + someArray[1]);
+        System.out.println(someArray.length);
+    }
+}
